4e9da3ae34d1995410d7eeefd5b28f5058a9f1d9
Chris Gardner <cgardner@gmail.com>
2016-05-23 21:58:37 -0500

    Emails on session added, updated, and deleted.

--- WebsiteAndApi/Controllers/SessionController.cs
+++ WebsiteAndApi/Controllers/SessionController.cs
@@ -31,13 +31,39 @@ namespace DevSpace.Api.Controllers {
 		[Authorize]
 		public async Task<HttpResponseMessage> Post( [ModelBinder( typeof( JsonSessionBinder ) )]ISession postedSession ) {
 			try {
+				IUser CurrentUser = ( Thread.CurrentPrincipal.Identity as DevSpaceIdentity )?.Identity;
+
 				if( -1 == postedSession.Id ) {
 					HttpResponseMessage Response = new HttpResponseMessage( HttpStatusCode.Created );
 					Response.Content = new StringContent( await Task.Factory.StartNew( () => JsonConvert.SerializeObject( _DataStore.Add( postedSession ).Result, Formatting.None ) ) );
+
+					Email Mail = new Email( CurrentUser.EmailAddress, CurrentUser.DisplayName );
+					Mail.Subject = "Session Submitted: " + postedSession.Title;
+					Mail.Body = string.Format(
+@"This message is to confirm the submission of your session: {0}.
+
+You may make changes to your session until June 24th.
+
+You should receive an email with the status of your submission on or around July 1st.",
+						postedSession.Title );
+					Mail.Send();
+
 					return Response;
 				} else {
 					HttpResponseMessage Response = new HttpResponseMessage( HttpStatusCode.NoContent );
 					Response.Content = new StringContent( await Task.Factory.StartNew( () => JsonConvert.SerializeObject( _DataStore.Update( postedSession ).Result, Formatting.None ) ) );
+
+					Email Mail = new Email( CurrentUser.EmailAddress, CurrentUser.DisplayName );
+					Mail.Subject = "Session Updated: " + postedSession.Title;
+					Mail.Body = string.Format(
+@"This message is to confirm the update of your session: {0}.
+
+You may continue to make changes to your session until June 24th.
+
+You should receive an email with the status of your submission on or around July 1st.",
+						postedSession.Title );
+					Mail.Send();
+
 					return Response;
 				}
 			} catch {
@@ -52,14 +78,22 @@ namespace DevSpace.Api.Controllers {
 			if( null == sessionToDelete )
 				return new HttpResponseMessage( HttpStatusCode.NotFound );
 
-			if( !sessionToDelete.UserId.Equals( ( Thread.CurrentPrincipal.Identity as DevSpaceIdentity )?.Identity.Id ?? -1 ) )
+			IUser CurrentUser = ( Thread.CurrentPrincipal.Identity as DevSpaceIdentity )?.Identity;
+			if( !sessionToDelete.UserId.Equals( CurrentUser?.Id ?? -1 ) )
 				return new HttpResponseMessage( HttpStatusCode.Unauthorized );
 
 			try {
-				if( await _DataStore.Delete( Id ) )
+				if( await _DataStore.Delete( Id ) ) {
+					Email Mail = new Email( CurrentUser.EmailAddress, CurrentUser.DisplayName );
+					Mail.Subject = "Session Deleted";
+					Mail.Body = string.Format(
+@"This message is to confirm the deletion of your session: {0}",
+						sessionToDelete.Title );
+					Mail.Send();
 					return new HttpResponseMessage( HttpStatusCode.NoContent );
-				else
+				} else {
 					return new HttpResponseMessage( HttpStatusCode.InternalServerError );
+				}
 			} catch {
 				return new HttpResponseMessage( HttpStatusCode.InternalServerError );
 			}
